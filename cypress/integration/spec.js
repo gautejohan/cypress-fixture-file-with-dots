f404bb66914a7f16329dafa2f79a0e36ad72e80f
gautejohan <gautej.johansen@gmail.com>
2018-03-02 12:15:06 +0100

    Add failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,12 @@
-describe('page', () => {
-  it('works', () => {})
+describe('Cypress fixture', () => {
+  it('should handle fixture name without prefix', () => {
+    cy.fixture('usertest1').then((json) => {
+      console.log('Finds file without extension with one dot');
+    })
+  });
+  it('should handle several dots in fixture name', () => {
+    cy.fixture('user.test.1').then((json) => {
+      console.log(json);
+    });
+  })
 })
